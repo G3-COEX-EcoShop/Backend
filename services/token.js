7c7517f8f2ff3089cd903279959089d2a8bb261a
Mauricio Arias <50003142+ImCrisam@users.noreply.github.com>
2023-04-06 17:00:15 -0500

    add .env secret key token

--- services/token.js
+++ services/token.js
@@ -1,73 +1,80 @@
-var jwt = require('jsonwebtoken');
-const models = require('../models');
+require("dotenv").config();
+
+var jwt = require("jsonwebtoken");
+const models = require("../models");
 
 async function checkToken(token) {
-    let id = null;
-    try {
-        const {
-            id
-        } = await jwt.decode(token);
-        id = id;
-    } catch (e) {
-        return false;
-    }
-    
-    const user = await models.Usuario.findOne({
-        where: {
-            id: id,
-            estado: 1
-        }
-    });
-    if (user) {
-        const token = jwt.sign({
-            id: id
-        }, 'secretKeyToGenerateToken', { //TODO: SecretKey
-            expiresIn: '1d'
-        });
-        return {
-            token,
-            rol: user.rol
-        };
-    } else {
-        return false;
-    }
+  let id = null;
+  try {
+    const { id } = await jwt.decode(token);
+    id = id;
+  } catch (e) {
+    return false;
+  }
+
+  const user = await models.Usuario.findOne({
+    where: {
+      id: id,
+      estado: 1,
+    },
+  });
+  if (user) {
+    const token = jwt.sign(
+      {
+        id: id,
+      },
+      process.env.SECRET_KEY_TO_GENERATE_TOKEN,
+      {
+        //TODO: SecretKey
+        expiresIn: "1d",
+      }
+    );
+    return {
+      token,
+      rol: user.rol,
+    };
+  } else {
+    return false;
+  }
 }
 
 module.exports = {
-
-    //generar el token
-    encode: async (id, rol,nombre,email) => {
-        
-        const token = jwt.sign({
-            id: id,
-            rol: rol,
-            nombre: nombre,
-            email: email
-        }, 'secretKeyToGenerateToken', {
-            expiresIn: '1d'
-        });
-        return token;
-    },
-    //permite decodificar el token
-    decode: async (token) => {
-        try {
-            const {
-                id
-            } = await jwt.verify(token, 'secretKeyToGenerateToken');
-            const user = await models.Usuario.findOne({
-                where: {
-                    id: id
-                }
-            });
-            if (user) {
-                return user;
-            } else {
-                return false;
-            }
-        } catch (e) {
-            const newToken = await checkToken(token);
-            return newToken;
-        }
-
+  //generar el token
+  encode: async (id, rol, nombre, email) => {
+    const token = jwt.sign(
+      {
+        id: id,
+        rol: rol,
+        nombre: nombre,
+        email: email,
+      },
+      process.env.SECRET_KEY_TO_GENERATE_TOKEN,
+      {
+        expiresIn: "1d",
+      }
+    );
+    return token;
+  },
+  //permite decodificar el token
+  decode: async (token) => {
+    try {
+      const { id } = await jwt.verify(
+        token,
+        process.env.SECRET_KEY_TO_GENERATE_TOKEN
+      );
+      const user = await models.Usuario.findOne({
+        where: {
+          id: id,
+        },
+      });
+      if (user) {
+        return user;
+      } else {
+        return false;
+      }
+    } catch (e) {
+      const newToken = await checkToken(token);
+      return newToken;
     }
-}
+  },
+};

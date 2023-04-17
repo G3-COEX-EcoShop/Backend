07faa9a8ef76d38e94329125d5eeefd26ad9a438
Mauricio Arias <50003142+ImCrisam@users.noreply.github.com>
2023-04-17 09:55:13 -0500

    config cookies

--- controllers/authController.js
+++ controllers/authController.js
@@ -58,7 +58,7 @@ module.exports = {
       }
 
       const tokenReturn = await token.encode(info.user);
-      res.cookie("token", tokenReturn);
+      res.cookie("token", tokenReturn, { sameSite: "none", secure: true });
       res.status(200).json({
         name: info.user.name,
       });
@@ -88,7 +88,7 @@ module.exports = {
       }
       if (info) {
         const tokenReturn = await token.encode(info.user);
-        res.cookie("token", tokenReturn);
+        res.cookie("token", tokenReturn, { sameSite: "none", secure: true });
       }
     } catch (error) {
       console.log(error);
